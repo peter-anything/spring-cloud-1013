2d0229136ea2e82888db8feaf6e451460978a308
peter.wang <wangxiaobin@datagrand.com>
2023-11-24 19:57:07 +0800

    update

--- dubbo-user-service-consumer/src/main/java/com/macro/cloud/dubbo/controller/UserController.java
+++ dubbo-user-service-consumer/src/main/java/com/macro/cloud/dubbo/controller/UserController.java
@@ -6,13 +6,17 @@ import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.websocket.server.PathParam;
+
 @RestController
 public class UserController {
-    @DubboReference
+    @DubboReference(version = "1.0")
     private UserService userService;
 
     @GetMapping("/user/{id}")
-    public User getUser(Long id) {
+    public User getUser(@PathParam("id") Long id) {
+        System.out.println(id);
+        System.out.println(userService);
         return userService.getUser(id);
     }
 }
