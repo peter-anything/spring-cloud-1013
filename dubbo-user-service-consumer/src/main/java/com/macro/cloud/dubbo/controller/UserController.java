afe400765fcce6d4c30019e6789b2b5420204c68
peter.wang <wangxiaobin@datagrand.com>
2023-11-24 19:58:54 +0800

    bugfix

--- dubbo-user-service-consumer/src/main/java/com/macro/cloud/dubbo/controller/UserController.java
+++ dubbo-user-service-consumer/src/main/java/com/macro/cloud/dubbo/controller/UserController.java
@@ -4,9 +4,8 @@ import com.macro.cloud.dubbo.domain.entity.User;
 import com.macro.cloud.dubbo.domain.service.UserService;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.websocket.server.PathParam;
 
 @RestController
 public class UserController {
@@ -14,7 +13,7 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/user/{id}")
-    public User getUser(@PathParam("id") Long id) {
+    public User getUser(@PathVariable("id") Long id) {
         System.out.println(id);
         System.out.println(userService);
         return userService.getUser(id);
